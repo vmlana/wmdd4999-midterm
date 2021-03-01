cfbe06ed421da5a0081b920e019d6213c74bc514
vmlana <vmlana@gmail.com>
2021-02-28 23:41:17 -0800

    search component

--- src/services/api.js
+++ src/services/api.js
@@ -2,14 +2,56 @@ import axios from 'axios'
 
 import { API_KEY, BASE_URL } from '../config/api_config'
 
-export const getMovies = async recipeName => {
-  const url = `${BASE_URL}/movie/now_playing?api_key=${API_KEY}`
+export const getMovies = async (origin, category, keyword) => {
+
+  let endpoint = ''
+  if (origin === 'movie') {
+    if (category === 0) {
+      endpoint = 'movie/now_playing'
+    } else if (category === 1) {
+      endpoint = 'movie/popular'
+    } else if (category === 2) {
+      endpoint = 'movie/top_rated'
+    } else if (category === 3) {
+      endpoint = 'movie/upcoming'
+    } else {
+      endpoint = 'movie/now_playing'
+    }
+  } else if (origin === 'tv') {
+    if (category === 0) {
+      endpoint = 'tv/airing today'
+    } else if (category === 1) {
+      endpoint = 'tv/on_the_air'
+    } else if (category === 2) {
+      endpoint = 'tv/popular'
+    } else if (category === 3) {
+      endpoint = 'tv/top_rated'
+    } else {
+      endpoint = 'tv/airing_today'
+    }
+  } else {
+    if (category === 0) {
+      endpoint = 'search/multi'
+    } else if (category === 1) {
+      endpoint = 'search/movie'
+    } else if (category === 2) {
+      endpoint = 'search/tv'
+    } else {
+      endpoint = 'search/multi'
+    }
+  }
+
+
+  let url = '';
+  if (keyword === undefined || keyword === '') {
+    url = `${BASE_URL}/${endpoint}?api_key=${API_KEY}`
+  } else {
+    url = `${BASE_URL}/${endpoint}?api_key=${API_KEY}&query=${keyword}`
+  }
+
   try {
     const response = await axios.get(url)
-
     const movies = response.data.results
-
-    console.log(movies);
 
     return movies
   } catch (error) {
@@ -23,9 +65,6 @@ export const getConfig = async config => {
   try {
     const response = await axios.get(url)
     const config = response.data
-    
-    console.log(config);
-
     return config
   } catch (error) {
     throw error
